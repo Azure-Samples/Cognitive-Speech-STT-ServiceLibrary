66017d8eaa97a068c943cc1b3b9da6da91ccef6e
jin wang <v-jiwag@microsoft.com>
2017-04-19 13:37:59 +0800

    remove app_start dir

---
+++ SpeechLuisOwin/SpeechLuisOwin/Src/Ext/Http.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Web.Http;
+
+namespace SpeechLuisOwin.Src.Ext
+{
+    public static class Http
+    {
+        public static void UseRoute(this IAppBuilder app, Func<HttpConfiguration, HttpConfiguration> action)
+        {
+            var config = new HttpConfiguration();
+            var con = action(config);
+            app.UseWebApi(con);
+        }
+    }
+}
--- SpeechLuisOwin/SpeechLuisOwin/Startup.cs
+++ SpeechLuisOwin/SpeechLuisOwin/Startup.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Owin;
-using Microsoft.Owin.Security.ActiveDirectory;
-using Owin;
-using SpeechLuisOwin.Src.Static;
-using System.Configuration;
-using System.IdentityModel.Tokens;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using SpeechLuisOwin.Src.Ext;
+using SpeechLuisOwin.Src.Media;
 
 [assembly: OwinStartup(typeof(SpeechLuisOwin.Startup))]
 namespace SpeechLuisOwin
@@ -13,28 +12,22 @@ namespace SpeechLuisOwin
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
+            app.UseAuth();
+
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-            app.UseWebApi(config);
-        }
 
-        public void ConfigureAuth(IAppBuilder app)
-        {
-            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
-                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
-                {
-                    // Audience = Configurations.aad_ClientId,
-                    Tenant = Configurations.aad_Tenant,
-                    TokenValidationParameters = new TokenValidationParameters
-                    {
-                        // wrong here because Client ID is not setted as audience
-                        // ValidAudience = Configurations.aad_ClientId
-                        ValidAudience = Configurations.aad_Resource
-                    }
-                });
-        }
+            app.UseRoute((config) => {               
+                config.MapHttpAttributeRoutes();
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
 
+                config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+                config.Formatters.Add(new BinaryMediaTypeFormatter());
+                return config;
+            });
+        }
     }
 }
